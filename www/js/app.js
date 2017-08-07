e82590d48d4a3ee17edf3402fe16fbc0f3efa531
mohammadkamil <mohammadkamil1991@gmail.com>
2017-08-07 09:49:41 +0800

    sds

---
+++ www/js/app.js
@@ -0,0 +1,43 @@
+/*
+ * Please see the included README.md file for license terms and conditions.
+ */
+
+
+// This file is a suggested starting place for your code.
+// It is completely optional and not required.
+// Note the reference that includes it in the index.html file.
+
+
+/*jslint browser:true, devel:true, white:true, vars:true */
+/*global $:false, intel:false app:false, dev:false, cordova:false */
+
+
+
+// This file contains your event handlers, the center of your application.
+// NOTE: see app.initEvents() in init-app.js for event handler initialization code.
+
+// function myEventHandler() {
+//     "use strict" ;
+// // ...event handler code here...
+// }
+
+
+// ...additional event handlers here...
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("48ebf8fc-c18b-4fcc-8dee-092fa1cd8908")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
+window.plugin.backgroundMode.enable();
